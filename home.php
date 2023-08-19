54c2bd44f92d353f67021d21202267be26467cc5
Abdalla-Ashraf-M7 <abdallaashrafm7@gmail.com>
2023-08-19 06:21:56 +0300

    meets_items_screen_finished

---
+++ home.php
@@ -0,0 +1,24 @@
+<?php
+include "connect.php";
+
+$alldata = array();
+
+$alldata['status'] = "success";
+
+$cats= getAllData("cats",null,null,false);
+$cats=json_decode($cats,true);
+
+$items= getAllData("itemsview","items_discount!=0",null,false);
+$items=json_decode($items,true);
+
+if($cats["status"]=="success"){
+    $alldata['cats']=$cats['data'];
+    if($items["status"]=="success"){
+        $alldata['items']=$items['data'];
+    }else{
+        printFailure("something worng with items");
+    }
+    echo json_encode($alldata);
+}else{
+    printFailure("something worng with categories");
+} 
---
+++ items/items.php
@@ -0,0 +1,4 @@
+<?php
+include "../connect.php";
+
+ getAllData("itemsview",null,null,true);
